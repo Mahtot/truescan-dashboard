b9164887e1d430522dbb262073be21d6652422ea
[mahtot] <mahtotgher13@gmail.com>
2025-05-15 10:03:16 +0300

    dependency conflicts fix

--- app/verify-otp/page.tsx
+++ app/verify-otp/page.tsx
@@ -3,7 +3,7 @@
 import OtpVerify from "@/components/forms/OtpVerify"
 import Image from "next/image"
 import logo from "@/public/logo.png"
-import { useEffect } from "react"
+import { Suspense, useEffect } from "react"
 import { useRouter } from "next/navigation"
 
 const VeriftyOtp = () => {
@@ -11,26 +11,21 @@ const VeriftyOtp = () => {
     const navigateTo = useRouter()
 
 
-    // useEffect(() => {
-    //     const allowed = sessionStorage.getItem('otpAllowed')
-    //     if (!allowed)
-    //         navigateTo.push('/signup')
-    //     else {
-    //         sessionStorage.removeItem('otpAllowed')
-    //     }
-    // }, [])
+
 
     return (
-        <div className="min-h-screen flex items-center justify-center ">
-            <div className="flex flex-col items-center justify-center w-max p-5 rounded-sm shadow-2xl">
-                <Image
-                    alt="Truescan logo"
-                    src={logo}
-                    className="w-24 h-24 object-contain rounded-b-2xl"
-                />
-                <OtpVerify />
+        <Suspense>
+            <div className="min-h-screen flex items-center justify-center ">
+                <div className="flex flex-col items-center justify-center w-max p-5 rounded-sm shadow-2xl">
+                    <Image
+                        alt="Truescan logo"
+                        src={logo}
+                        className="w-24 h-24 object-contain rounded-b-2xl"
+                    />
+                    <OtpVerify />
+                </div>
             </div>
-        </div>
+        </Suspense>
     )
 }
 
